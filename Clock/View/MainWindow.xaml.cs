236b707b225a4598dfd7a2ec45a9a34a65e05140
Franzi <dsfranzi@web.de>
2014-12-02 21:43:14 +0100

    MainWindow verschoben nach /View

--- Clock/View/MainWindow.xaml.cs
+++ Clock/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Clock {
+namespace Clock.View {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
